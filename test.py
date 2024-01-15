16fe9417d24cb1b8c256d8ca23b1865e814648c7
Sachin <sachnaror@users.noreply.github.com>
2024-01-16 05:02:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = 'sk-7LWn4p9jQQoo5dgAwLAsT3BlbkFJ2N625TTPBmEQMVjqT'
 
 model = OpenAI(temperature=0)
 chain = ConversationChain(llm=model,
